efd4fc80ec8f5433c3eeb58bf2420ed978b80377
Mariana Naselli <nasellimariana@outlook.com>
2025-01-12 16:58:47 -0300

    🛠️ angular.json -> Added Bootstrap CSS and JS files 🛠️ src/app/app.component.html -> Commented out navigation links 🔴 src/app/app.module.ts -> File removed 🟢 src/app/components/task-form/task-form.component.html -> Added task form modal 🛠️ src/app/components/task-form/task-form.component.ts -> Updated to handle task form submission 🛠️ src/app/components/task-list/task-list.component.html -> Modified task list layout and added button to show modal

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-task-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.css'
 })
 export class TaskFormComponent {
+  @Output() addTask = new EventEmitter<any>();
 
+  task = {
+    title: '',
+    category: '',
+    deadline: null,
+  };
+
+  onSubmit() {
+    if (this.task.title) {
+      this.addTask.emit(this.task);
+      this.task = { title: '', category: '', deadline: null }; // Reset form
+      const modalElement = document.getElementById('addTaskModal') as HTMLElement;
+      // const modal = bootstrap.Modal.getInstance(modalElement); // Cierra el modal con Bootstrap
+      // modal?.hide();
+    }
+  }
 }
